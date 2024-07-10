3170a42db877f1789f06d2d7b9643736f33128dd
middletaq@gmail.com <middletaq@gmail.com>
2024-07-09 23:00:26 -0400

    second

---
+++ basics.py
@@ -0,0 +1,16 @@
+# # Numbers: Simple Arithmetic
+# Write an expression that equals 100. 
+
+# For example 50+50   or 110-10 . 
+
+# See if you can use more than one arithmetic operator! Write only one expression and submit only one line of code.
+# An expression with the variable B
+b = 50 + 50
+print(b)
+
+
+a = 7         # assign variable a the value 7
+c = b         # assign variable c the value of b
+
+t = 'My name is Taquan'
+print(t)
